6680eb4349f48ca97cafb923b4cb4ad3e52848ff
CodeSolaris <volkov.aleksandr.it@gmail.com>
2024-10-07 12:28:03 +0300

    feat: emplement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from "fs/promises";
+
+const filePath = "./src/fs/files/fresh.txt";
+const fileContent = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.access(filePath);
+        throw new Error("FS operation failed");
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            await fs.writeFile(filePath, fileContent);
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
