d95cc0c4d3acb1fb039f5a0c4a904755fa2eeedc
CodeSolaris <volkov.aleksandr.it@gmail.com>
2024-10-07 15:07:29 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import fs from "fs/promises";
+
+const fileToRead = "./src/fs/files/fileToRead.txt";
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const content = await fs.readFile(fileToRead, "utf-8");
+        console.log(content);
+    } catch (error) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await read();
