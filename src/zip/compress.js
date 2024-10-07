64337cf6f15ca9fbf86186035aab1c10aa68ad4f
CodeSolaris <volkov.aleksandr.it@gmail.com>
2024-10-07 23:33:00 +0300

    feat: implement compress fumction

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
-const compress = async () => {
-    // Write your code here 
-};
+import fs from "fs";
+import zlib from "zlib";
 
+const fileToCompress = "./src/zip/files/fileToCompress.txt";
+const archiveName = "./src/zip/files/archive.gz";
+
+const compress = async () => {
+    const readStream = fs.createReadStream(fileToCompress);
+    const writeStream = fs.createWriteStream(archiveName);
+    const gzip = zlib.createGzip();
+    readStream.pipe(gzip).pipe(writeStream);
+};
 await compress();
