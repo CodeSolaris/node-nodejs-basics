ef92a84ff9d7ab5915c41e9553ccb8eb8f47b7d0
CodeSolaris <volkov.aleksandr.it@gmail.com>
2024-10-07 14:55:34 +0300

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import fs from "fs/promises";
+
+const srcFolder = "./src/fs/files";
+const destFolder = "./src/fs/files_copy";
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        // Check if the source folder exists
+        await fs.access(srcFolder);
+
+        try {
+            // Check if the destination folder already exists
+            await fs.access(destFolder);
+            throw new Error("FS operation failed");
+        } catch (error) {
+            if (error.code === "ENOENT") {
+                await fs.cp(srcFolder, destFolder, { recursive: true });
+            } else {
+                throw error;
+            }
+        }
+    } catch (error) {
+        // If the source folder doesn't exist (ENOENT), throw an error
+        if (error.code === "ENOENT") {
+            throw new Error("FS operation failed");
+        } else {
+            throw error;
+        }
+    }
 };
 
 await copy();
