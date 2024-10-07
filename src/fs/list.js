ac13eaf151c87f8ed78fd62e9d0c95c181632fdc
CodeSolaris <volkov.aleksandr.it@gmail.com>
2024-10-07 15:12:03 +0300

    feat: implement list dir function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import fs from "fs/promises";
+
+const folderToRead = "./src/fs/files";
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const files = await fs.readdir(folderToRead);
+        console.log(...files);
+    } catch (error) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await list();
