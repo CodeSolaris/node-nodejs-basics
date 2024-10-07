6b4422f1edc4ec7d4e356a8122e8e3aee234ef99
CodeSolaris <volkov.aleksandr.it@gmail.com>
2024-10-07 15:04:30 +0300

    feat: implement delete file function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+
+const fileToRemove = "./src/fs/files/fileToRemove.txt";
 const remove = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        await fs.unlink(fileToRemove);
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            throw new Error("FS operation failed");
+        } else {
+            throw error;
+        }
+    }
 };
 
 await remove();
