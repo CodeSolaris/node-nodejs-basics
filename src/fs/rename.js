e5b6a7f1659773aa6a641a1af1c6ea594b6ee20e
CodeSolaris <volkov.aleksandr.it@gmail.com>
2024-10-07 17:20:46 +0300

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import fs from "fs/promises";
+
+const dirToFiles = "./src/fs/files/";
+const fileToRename = dirToFiles + "wrongFilename.txt";
+const newFileName = dirToFiles + "properFilename.md";
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await fs.access(fileToRename);
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            throw new Error("FS operation failed");
+        } else {
+            throw error;
+        }
+    }
+
+    try {
+        await fs.access(newFileName);
+        throw new Error("FS operation failed");
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            await fs.rename(fileToRename, newFileName);
+        } else {
+            throw error;
+        }
+    }
 };
 
 await rename();
