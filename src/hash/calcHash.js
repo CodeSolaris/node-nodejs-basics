d653fb3221dd6a69d270183fe02eba7440b77461
CodeSolaris <volkov.aleksandr.it@gmail.com>
2024-10-07 22:49:15 +0300

    feat: implement calculate hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
 
+const fileToCalculateHash = "./src/hash/files/fileToCalculateHashFor.txt";
+
+export const calculateHash = async () => {
+    const fileStream = createReadStream(fileToCalculateHash);
+    const hash = createHash("sha256");
+
+    for await (const chunk of fileStream) {
+        hash.update(chunk);
+    }
+
+    const fileHash = hash.digest("hex");
+    console.log(fileHash);
+    return fileHash;
+};
 await calculateHash();
